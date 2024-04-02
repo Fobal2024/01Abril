143a263cf33976a367a9a76d37332f30892785fe
Alumno <Alumno@LAB1SSDPC2>
2024-04-01 21:16:06 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+    static void Main()
+        // Repartido 1 Ejercicio 1
+    {
+        /* int entero = 1;
+         float flotante = entero;
+
+         Console.WriteLine("Flotante = " + entero);
+
+        // Repartido 1 Ejercicio 9
+
+        float flotante = 1.24f;
+        int entero = Convert.ToInt32(flotante);
+
+        Console.WriteLine("Entero = " + entero);*/
+    }
+}
+
